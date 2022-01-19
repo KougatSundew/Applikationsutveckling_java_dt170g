d947e94686897c3e77f1c7a8ba9bd99512d9df7a
Joel Olofsson <57771808+KougatSundew@users.noreply.github.com>
2022-01-19 10:54:13 +0100

    Init labb1

---
+++ Labb1/app/src/main/java/com/jool1904/labb1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jool1904.labb1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
