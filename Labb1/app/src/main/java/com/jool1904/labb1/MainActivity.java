0e97cd1de52031406203e7c2b5b0500e9fff21b4
Joel Olofsson <57771808+KougatSundew@users.noreply.github.com>
2022-01-21 11:11:48 +0100

    Init work lab 1

--- Labb1/app/src/main/java/com/jool1904/labb1/MainActivity.java
+++ Labb1/app/src/main/java/com/jool1904/labb1/MainActivity.java
@@ -3,6 +3,8 @@ package com.jool1904.labb1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button refreshBtn = findViewById(R.id.btnRefresh);
+        refreshBtn.setOnClickListener(view -> {
+            Log.i(refreshBtn.getClass().toString(), "Refresh button clicked");
+        });
     }
 }
---
+++ Labb1/app/src/main/java/com/jool1904/labb1/WeatherService.java
@@ -0,0 +1,8 @@
+package com.jool1904.labb1;
+
+public class WeatherService {
+    private static final String URL_WEATHER = "";
+    public WeatherService() {
+
+    }
+}
